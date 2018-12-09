ba7b9b03aea229b7592033ccd4040240fac91fd1
PavankumarAN <pavankumaran51@gmail.com>
2018-12-09 10:44:30 +0530

    Web

---
+++ JavaScript/Assignment1.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Please enter your first name");
+var secondName = prompt("Please enter your second name");
+var age = prompt("Please enter your age");
+var userName = firstName+ " " +secondName;
+alert("The age of the "+userName+" is "+age);
+console.log("The age of the "+userName+" is "+age);
